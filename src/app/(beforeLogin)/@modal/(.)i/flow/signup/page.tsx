88eef8a5239af732420ed1c659ca0341daf1cd60
cha <chyb627@naver.com>
2023-12-20 14:45:27 +0900

    로그인 모달 문제 수정

--- src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -1,5 +1,10 @@
 import SignupModal from '@/app/(beforeLogin)/_component/SignupModal';
 
 export default function Signup() {
-  return <SignupModal />;
+  return (
+    <>
+      인터셉팅 라우트 페이지~
+      <SignupModal />
+    </>
+  );
 }
