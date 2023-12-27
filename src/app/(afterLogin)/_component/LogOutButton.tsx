054a471680e558a0bbff062a36232588b32de51c
cha <chyb627@naver.com>
2023-12-27 13:25:24 +0900

    useSelectedLayoutSegment, Active Link 만들기

---
+++ src/app/(afterLogin)/_component/LogOutButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import style from './logoutButton.module.css';
+
+export default function LogOutButton() {
+  const me = {
+    // 임시로 내 정보 있는것처럼
+    id: 'test',
+    nickname: '테스트',
+    image: '/profile.png',
+  };
+
+  const onLogout = () => null;
+
+  return (
+    <button className={style.logOutButton} onClick={onLogout}>
+      <div className={style.logOutUserImage}>
+        <img src={me.image} alt={me.id} />
+      </div>
+      <div className={style.logOutUserName}>
+        <div>{me.nickname}</div>
+        <div>@{me.id}</div>
+      </div>
+    </button>
+  );
+}
